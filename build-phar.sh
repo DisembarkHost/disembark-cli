b8b41f05bd708bf71b31c8dcdd5132b9490c77d1
Austin Ginder <austinginder@gmail.com>
2025-10-10 07:27:01 -0400

    👌 IMPROVE: Build script

--- build-phar.sh
+++ build-phar.sh
@@ -1,6 +1,6 @@
+if [ -f disembark.phar ]; then 
+    rm disembark.phar
+fi
 echo "Building disembark.phar"
 php -d phar.readonly=0 build-phar.php
-
-echo "Installing disembark.phar to /usr/local/bin/disembark"
 chmod +x disembark.phar
-sudo mv disembark.phar /usr/local/bin/disembark
