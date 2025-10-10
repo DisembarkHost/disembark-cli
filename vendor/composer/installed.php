2cf856d08bbcc54d511e34ecedae3a63d905bbcc
Austin Ginder <austinginder@gmail.com>
2025-10-10 07:26:10 -0400

    👌 IMPROVE: Request library

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '7242ededb8a6b68b72781822a76ee688ac498af7',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '7242ededb8a6b68b72781822a76ee688ac498af7',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
